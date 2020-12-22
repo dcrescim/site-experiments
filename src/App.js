e347a13ee0651008ad93dc2058e3e5cf762e24a0
Dan Crescimanno <dcrmls@gmail.com>
2020-12-22 12:09:25 -0500

    Just a button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+function App({ isOrange }) {
+  let linkStyle = { fontSize: "72px", textDecoration: "none" }
+
+  if (isOrange) {
+    linkStyle.color = "orange"
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
+          style={linkStyle}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
